3ea283f8603c2c4caa2eaa55fae1168f04375bea
SayedEnam <syedenamshohag@gmail.com>
2020-02-20 11:26:49 +0600

    Updates

---
+++ precache-manifest.912e2a81be3a48e822ddd0f599ba7651.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e3a201411741074acc65d75cd11ab6cb",
+    "url": "/photo-gallery-api/index.html"
+  },
+  {
+    "revision": "85f0c705b83abc9b3402",
+    "url": "/photo-gallery-api/static/css/main.a7c07069.chunk.css"
+  },
+  {
+    "revision": "d7bda202ba6a5b6347ec",
+    "url": "/photo-gallery-api/static/js/2.22810154.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/photo-gallery-api/static/js/2.22810154.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "85f0c705b83abc9b3402",
+    "url": "/photo-gallery-api/static/js/main.a9d3d0fa.chunk.js"
+  },
+  {
+    "revision": "29f78b38de45488f5fe9",
+    "url": "/photo-gallery-api/static/js/runtime-main.1808c958.js"
+  }
+]);
